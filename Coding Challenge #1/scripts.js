7ebfa8c42c074673f2209b2a5cbd43bd3f526c8e
Md. Emam Uddin Shahed <shahedimamuddin@gmail.com>
2023-01-19 02:03:43 +0600

    Guess My Number Project

---
+++ Coding Challenge #1/scripts.js
@@ -0,0 +1,17 @@
+
+
+const massOfMark = 78;
+const heightOfMark = 1.69;
+const massOfJohn = 92;
+const heightOfJohn = 1.95;
+
+
+const BMIofMark = (massOfMark / heightOfMark ** 2);
+console.log(BMIofMark);
+
+const BMIofJohn = (massOfJohn / heightOfJohn ** 2);
+console.log(BMIofJohn);
+
+console.log(BMIofMark > BMIofJohn);
+
+
---
+++ Guess My Number Project/scripts.js
@@ -0,0 +1,7 @@
+
+
+document.querySelector('.check').addEventListener
+    ('click', function () {
+        const guess = Number(document.querySelector('.guess').value);
+        console.log(guess, typeof guess);
+    });
